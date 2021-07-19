61f4fcef355eaaf784719e5fd1f06573f0fbfd4c
Zakpak0 <ZakharyOliver808@gmail.com>
2021-07-18 15:52:33 -1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,4 +6,11 @@ module.exports = {
             "firebasestorage.googleapis.com",
         ],
     },
+    future: {
+    webpack5: true,
+    },
+    webpack: function (config, options) {
+    config.experiments = {};
+    return config;
+    },
 };
